b67969df86c549316c776f98e28e9a0b00c63844
Arpit Sharma <58043934+arpitsharmaji@users.noreply.github.com>
2025-02-17 00:07:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ setInterval(function () {
     setTimeout(() => {
         index = (index + 1) % images.length; // Move to next image in sequence
         image.src = images[index]; // Change image
-        image.style.opacity = 0; // Fade in effect
+        image.style.opacity = 1; // Fade in effect
     }, 0); // Change after fade-out completes
 
 }, 4000);
